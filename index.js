2ce6dc8c1952faeae3324532a0a8fdcbe6c5aaba
Johannes Haseitl <johannes@undpaul.de>
2014-02-04 22:21:20 +0100

    Get issue information with journal for each issue.

--- index.js
+++ index.js
@@ -3,15 +3,23 @@ var config = require('./config.json');
 
 var r = new redmine(config.host, config.port, config.key);
 
-var query = {
-  project_id: 329,
-  limit: 100,
-  offset: 0
-};
+// Hardcoded project id.
+var projectId = 329;
 
-r.getAll('issues', function(err, data) {
+var issues = [];
+
+r.getAll('issues', {project_id: projectId}, function(err, data) {
   if (err) {
     console.log('ERROR', err);
   }
-  console.log('Data', data.length);
+  console.log('Collected issues:', data.length);
+
+  for (var i in data) {
+    var d = data[i];
+
+    r.getSingle('issue', d.id, {include: 'journals'}, function(err, issue) {
+      issues.push(issue);
+      console.log('Fetched:', issue.project.name, issue.id);
+    });
+  }
 });
