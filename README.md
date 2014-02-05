271f84b53844bb8eae2e385ab4ae0b149d209275
Johannes Haseitl <johannes@undpaul.de>
2014-02-05 09:44:46 +0100

    Getting data in a given range for each status.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Redmine analysis
 
+This tool uses the [Redmine REST API](http://www.redmine.org/projects/redmine/wiki/Rest_api)
+for fetching data from a specific project and show some (currently one) graph.
+
 ## Installation
 
 ```
--- index.js
+++ index.js
@@ -8,13 +8,15 @@ var r = new redmine(config.host, config.port, config.key);
 // Hardcoded project id.
 var projectId = 329;
 
-
 var fromDate = new Date('2014-01-01');
 var toDate = Date.now();
 
 var dateRange = momentRange().range(fromDate, toDate);
+var dateRangeArray = [];
+
+// Iterate over date range.
 dateRange.by('days', function(moment) {
-  console.log('Moment', moment.toString());
+  dateRangeArray.push(moment);
 });
 
 redmineIssue.getAll(r, {project_id: projectId}, function(err, issues) {
@@ -23,8 +25,42 @@ redmineIssue.getAll(r, {project_id: projectId}, function(err, issues) {
   }
   console.log('Collected issues:', issues.length);
 
-  for (var i in issues) {
-    var issue = issues[i];
-    console.log(issue.id + ' - Status: ' + issue.getStatus(Date.now()));
-  }
+
+  // Prerun status, so it is present later on.
+  r.getStatuses(function(err, statuses) {
+
+    console.log('Statuses', statuses);
+
+    var series = {};
+
+    // We run through each
+    for (var i in issues) {
+
+      var issue = issues[i];
+
+      for (var day in dateRangeArray) {
+        var thisDate = dateRangeArray[day];
+
+        var status = issue.getStatus(thisDate);
+
+        // Only proceed if the issue has a valid status for that day.
+        if (status != undefined && statuses[status] != undefined) {
+
+          // Ensure status array is defined.
+          if (series[status] == undefined) {
+            series[status] = {};
+          }
+          // Ensure status for the day is present.
+          if (series[status][day] == undefined) {
+            series[status][day] = 0;
+          }
+
+          // Increment the status for that day for the given issue.
+          series[status][day]++;
+        }
+      }
+    }
+
+    console.log('Series', series);
+  });
 });
