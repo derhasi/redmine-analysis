8b0cb269789d7fd46201145775b70c4fbc184dcd
Johannes Haseitl <johannes@undpaul.de>
2014-02-05 10:14:56 +0100

    Use express to return data.

--- index.js
+++ index.js
@@ -5,75 +5,91 @@ var momentRange = require('moment-range');
 
 var r = new redmine(config.host, config.port, config.key);
 
-// Hardcoded project id.
-var projectId = 329;
+function getData(callback) {
+  // Hardcoded project id.
+  var projectId = 329;
 
-var fromDate = new Date('2014-01-01');
-var toDate = Date.now();
+  var fromDate = new Date('2014-01-01');
+  var toDate = Date.now();
 
-var dateRange = momentRange().range(fromDate, toDate);
-var dateRangeArray = [];
+  var dateRange = momentRange().range(fromDate, toDate);
+  var dateRangeArray = [];
 
-// Iterate over date range.
-dateRange.by('days', function(moment) {
-  dateRangeArray.push(moment);
-});
+  // Iterate over date range.
+  dateRange.by('days', function(moment) {
+    dateRangeArray.push(moment);
+  });
 
-redmineIssue.getAll(r, {project_id: projectId}, function(err, issues) {
-  if (err) {
-    console.log('ERROR', err);
-  }
-  console.log('Collected issues:', issues.length);
+  redmineIssue.getAll(r, {project_id: projectId}, function(err, issues) {
+    if (err) {
+      console.log('ERROR', err);
+    }
+    console.log('Collected issues:', issues.length);
 
+    // Prerun status, so it is present later on.
+    r.getStatuses(function(err, statuses) {
 
-  // Prerun status, so it is present later on.
-  r.getStatuses(function(err, statuses) {
+      console.log('Statuses', statuses);
 
-    console.log('Statuses', statuses);
+      var series = {};
 
-    var series = {};
+      // We run through each
+      for (var i in issues) {
 
-    // We run through each
-    for (var i in issues) {
+        var issue = issues[i];
 
-      var issue = issues[i];
+        for (var day in dateRangeArray) {
+          var thisDate = dateRangeArray[day];
 
-      for (var day in dateRangeArray) {
-        var thisDate = dateRangeArray[day];
+          var status = issue.getStatus(thisDate);
 
-        var status = issue.getStatus(thisDate);
+          // Only proceed if the issue has a valid status for that day.
+          if (status != undefined && statuses[status] != undefined) {
 
-        // Only proceed if the issue has a valid status for that day.
-        if (status != undefined && statuses[status] != undefined) {
+            // Ensure status array is defined.
+            if (series[status] == undefined) {
+              series[status] = {};
+            }
+            // Ensure status for the day is present.
+            if (series[status][day] == undefined) {
+              series[status][day] = 0;
+            }
 
-          // Ensure status array is defined.
-          if (series[status] == undefined) {
-            series[status] = {};
+            // Increment the status for that day for the given issue.
+            series[status][day]++;
           }
-          // Ensure status for the day is present.
-          if (series[status][day] == undefined) {
-            series[status][day] = 0;
-          }
-
-          // Increment the status for that day for the given issue.
-          series[status][day]++;
         }
       }
-    }
 
-    var rickshawSeries = [];
+      var rickshawSeries = [];
 
-    // Restructure data to rickshaw series.
-    for (var status in series) {
+      // Restructure data to rickshaw series.
+      for (var status in series) {
 
-      var data = [];
+        var data = [];
 
-      for (var day in series[status]) {
-        data.push({x: day, y: series[status][day]});
+        for (var day in series[status]) {
+          data.push({x: day, y: series[status][day]});
+        }
+        rickshawSeries.push({data: data});
       }
-      rickshawSeries.push({data: data});
-    }
 
-    console.log('Series', rickshawSeries);
+      console.log('Series', rickshawSeries);
+      callback(undefined, rickshawSeries);
+    });
+  });
+
+}
+
+
+var express = require('express');
+var app = express();
+
+app.get('/data.json', function(req, res){
+  getData(function(err, data) {
+    res.send(data);
   });
 });
+
+app.listen(3000);
+console.log('Listening on port 3000');
