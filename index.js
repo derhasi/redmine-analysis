c8776acfcdc41ca59f39ec7ea63de05f0ced9273
Johannes Haseitl <johannes@undpaul.de>
2014-02-05 08:22:39 +0100

    Implement date range with moment-range.
    @see https://github.com/gf3/moment-range

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 var redmine = require('./lib/redmine');
 var redmineIssue = require('./lib/redmine-issue');
 var config = require('./config.json');
+var momentRange = require('moment-range');
 
 var r = new redmine(config.host, config.port, config.key);
 
 // Hardcoded project id.
 var projectId = 329;
+
+
+var fromDate = new Date('2014-01-01');
+var toDate = Date.now();
+
+var dateRange = momentRange().range(fromDate, toDate);
+dateRange.by('days', function(moment) {
+  console.log('Moment', moment.toString());
+});
 
 redmineIssue.getAll(r, {project_id: projectId}, function(err, issues) {
   if (err) {
