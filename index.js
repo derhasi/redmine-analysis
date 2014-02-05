740b2dad65fb61547289aab2db24aad39d3cae61
Johannes Haseitl <johannes@undpaul.de>
2014-02-05 07:38:00 +0100

    Added static function to get All issues with a single callback.

--- index.js
+++ index.js
@@ -7,20 +7,14 @@ var r = new redmine(config.host, config.port, config.key);
 // Hardcoded project id.
 var projectId = 329;
 
-var issues = [];
-
-r.getAll('issues', {project_id: projectId}, function(err, data) {
+redmineIssue.getAll(r, {project_id: projectId}, function(err, issues) {
   if (err) {
     console.log('ERROR', err);
   }
-  console.log('Collected issues:', data.length);
+  console.log('Collected issues:', issues.length);
 
-  for (var i in data) {
-    var d = data[i];
-
-    r.getIssue(d.id, function(err, issue) {
-      console.log(issue.id + ' - Status: ' + issue.getStatus(Date.now()));
-      issues.push(issue);
-    });
+  for (var i in issues) {
+    var issue = issues[i];
+    console.log(issue.id + ' - Status: ' + issue.getStatus(Date.now()));
   }
 });
